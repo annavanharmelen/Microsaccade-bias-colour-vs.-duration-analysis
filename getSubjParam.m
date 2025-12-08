759d5075167376582f8882036df63733fc6ed54e
Anna <anna@vanharmelen.me>
2025-12-08 15:17:14 +0100

    add unique pp id's

--- getSubjParam.m
+++ getSubjParam.m
@@ -3,7 +3,7 @@ function param = getSubjParam(pp)
 %% participant-specific notes
 
 %% set path and pp-specific file locations
-unique_numbers = [12, 60, 94, 72]; %needs to be in the right order
+unique_numbers = [12, 60, 94, 72, 57, 31, 21, 91, 41, 19, 67, 49, 24, 79, 42, 59, 93, 45, 95, 18, 77, 32, 58, 90, 35]; %needs to be in the right order
 
 param.path = '\\scistor.vu.nl\shares\FGB-ETP-CogPsy-ProactiveBrainLab\core_lab_members\Laurie\duration_color\';
 
